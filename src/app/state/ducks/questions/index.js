48999b86e780a4ee47794e03355e13aecbb4020c
Samuel Will <samuelwill@linux.fritz.box>
2020-03-31 00:49:21 +0200

    chore: change directories

---
+++ src/app/state/ducks/questions/index.js
@@ -0,0 +1,8 @@
+import reducer from './reducers';
+
+import * as questionOperations from './operations';
+import * as questionSelectors from './selectors';
+
+export { questionOperations, questionSelectors };
+
+export default reducer;
