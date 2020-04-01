74daf0ce0b25013bc34d8712d2e7e98bc53b55a7
Samuel Will <samuelwill@linux.fritz.box>
2020-04-01 22:16:27 +0200

    feature: Userdata Card

--- src/app/views/Question/Question.react.js
+++ src/app/views/Question/Question.react.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { Container, Background, ProfilePicture, PictureBorder, Header } from './Question.styled';
+import PropTypes from 'prop-types';
+import {
+  Container,
+  Background,
+  UserData,
+  QuestionBy,
+  Name,
+  ProfilePicture,
+  PictureBorder,
+  Header,
+} from './Question.styled';
 import useWindowDimensions from './Question.helper';
 
-const Question = () => {
-  const url = 'https://i.pinimg.com/564x/c3/98/41/c398410b49f5a52a7b566c3f9a9c0bf6.jpg';
+const Question = ({ url }) => {
   const { width } = useWindowDimensions();
   return (
     <Background width={width}>
-      <PictureBorder>
-        <ProfilePicture url={url} />
-      </PictureBorder>
+      <UserData>
+        <PictureBorder>
+          <ProfilePicture url={url} />
+        </PictureBorder>
+        <QuestionBy>
+          <i>Question By:</i>
+          <br />
+          <Name>
+            <b>Samuel</b>
+          </Name>
+        </QuestionBy>
+      </UserData>
       <Container>
         <Header>Hello</Header>
         <Header>Hello</Header>
       </Container>
     </Background>
   );
+};
+
+Question.propTypes = { url: PropTypes.string };
+// TODO: REMOVE BEFORE PUBLISH
+Question.defaultProps = {
+  url: 'https://i.pinimg.com/564x/c3/98/41/c398410b49f5a52a7b566c3f9a9c0bf6.jpg',
 };
 
 export default Question;
--- src/app/views/Question/Question.styled.js
+++ src/app/views/Question/Question.styled.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 
 export const Background = styled.div`
@@ -8,8 +9,9 @@ export const Background = styled.div`
   margin: auto;
 // width: ${(props) => (props.width * 0.6 > 800 ? 800 : props.width * 0.6)}px;
   width:60%;
+  min-width: 500px;
   max-width: 800px;
-
+  height: 180px;
   margin: auto;
   display: flex;
   align-items: center;
@@ -19,15 +21,31 @@ export const Background = styled.div`
 
 /* --------------------------------- Picture -------------------------------- */
 
+export const UserData = styled.div`
+  flex-grow: 1;
+  margin: auto;
+  //   margin: 15px 25px;
+`;
+export const QuestionBy = styled.p`
+  margin: 0px;
+  margin-block-start: 0px;
+  margin-block-end: 0px;
+  color: #607d8b;
+`;
+export const Name = styled.span`
+  color: #37474f;
+`;
+
 export const PictureBorder = styled.div`
   width: 120px;
   height: 120px;
+  margin: auto;
   border-radius: 100px;
-  margin: 15px 25px;
+  //   margin-top: 15px;
 
-  max-width: 250px;
-  padding: 1rem;
-  position: relative;
+  //   max-width: 250px;
+  //   padding: 1rem;
+  //   position: relative;
   background-color: #21d4fd;
   background-image: linear-gradient(130deg, #21d4fd 0%, #b721ff 100%);
   padding: 3px;
@@ -39,14 +57,13 @@ export const ProfilePicture = styled.div`
   border-radius: 100px;
   background-image: url('${(props) => props.url}');
   background-size: contain;
-
 `;
 
 /* -------------------------------- Question -------------------------------- */
 
 export const Container = styled.div`
   display: flex;
-  flex-grow: 2;
+  flex-grow: 4;
   width: 100px;
   height: 100px;
   margin-right: 25px;
@@ -58,5 +75,4 @@ export const Header = styled.h3`
   color: #abab;
   left: 0
   position: absolute;
-//   align-self: flex-start;
 `;
