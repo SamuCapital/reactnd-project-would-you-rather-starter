eaa14692efed9c98ca3df3f0859d95414e66b369
Samuel <bok5@gmx.net>
2020-04-08 20:15:02 +0200

    Dev: Adding Redux Devtools

--- src/app/state/store.js
+++ src/app/state/store.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { loadingBarReducer } from 'react-redux-loading';
 import * as reducers from './ducks';
 import sessionReducer from './ducks/Session/reducers'; // TODO: REMOVE ONCE NEXT REDUCER IS READY
 import middleware from './Middleware';
 
 const rootReducer = combineReducers(reducers, loadingBarReducer);
-const Store = createStore(rootReducer, middleware);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const Store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(middleware));
 
 export default Store;
