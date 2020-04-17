344711c3c07ffeada19518c1dfe3d9211813911b
Samuel <bok5@gmx.net>
2020-04-17 06:23:31 +0200

    Updates

---
+++ precache-manifest.d96581da932f38dbbe7b77473c950a9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a90dce049491169f56095317672962b",
+    "url": "/reactnd-project-would-you-rather-starter/index.html"
+  },
+  {
+    "revision": "3d644babaad727d8f9ca",
+    "url": "/reactnd-project-would-you-rather-starter/static/css/2.b269c6b2.chunk.css"
+  },
+  {
+    "revision": "80b5c0c3c2b1acfca03e",
+    "url": "/reactnd-project-would-you-rather-starter/static/css/main.31bfced9.chunk.css"
+  },
+  {
+    "revision": "3d644babaad727d8f9ca",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/2.c9bb383b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/2.c9bb383b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b5c0c3c2b1acfca03e",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/main.228f121e.chunk.js"
+  },
+  {
+    "revision": "cfbefeeff1433d21eafb",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/runtime-main.bcf3dfff.js"
+  }
+]);
